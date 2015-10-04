4ec7505562c072e3540d8fe1d320bfbdf1c095c6
loredanasilaghi <loredana.silaghi@gmail.com>
2015-10-04 14:26:19 +0300

    Completed solution for problem with encryption/decryption

--- Tema2/EncryptionProblem.cs
+++ Tema2/EncryptionProblem.cs
@@ -6,104 +6,101 @@ using System.Collections.Generic;
 namespace Training
 {
     [TestClass]
-    public class EncryptionProblemMatrix
+    public class EncryptionProblem
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleMessage()
         {
-            string originalMsg = "nicaierinuecaacasa";
-            string encryptedMsg = EncryptMessage(originalMsg, 4);
+            string originalMsg = "nicaieri nu e ca acasa";
+            string expectedMsg = "nicaierinuecaacasa";
+            Message encryptedMsg = EncryptMessage(originalMsg, 4);
             string decryptedMsg = DecryptMessage(encryptedMsg, 4);
-            Assert.AreEqual(originalMsg, decryptedMsg);
+            Assert.AreEqual(expectedMsg, decryptedMsg);
         }
-        //remove last 3 chars
-        //str = str.Remove(str.Length - 3);
 
-        private static string EncryptMessage(string message, int columns)
+        [TestMethod]
+        public void ComplexMessage()
         {
-            int rows = 0;
-            char[][] matrix = SetOriginalMsgIntoColumns(message, columns, ref rows);
-            string encryptedMsg = string.Empty;
+            string originalMsg = "era mai frumos, daca nu stergeam spatiile, nu?";
+            string expectedMsg = "eramaifrumosdacanustergeamspatiilenu";
+            Message encryptedMsg = EncryptMessage(originalMsg, 4);
+            string decryptedMsg = DecryptMessage(encryptedMsg, 4);
+            Assert.AreEqual(expectedMsg, decryptedMsg);
+        }
+
+        private struct Message
+        {
+            public string msg;
+            public int extraChar;
+            public Message(string msg_var, int extrachar_var)
+            {
+                msg = msg_var;
+                extraChar = extrachar_var;
+            }
+        }
+       
+        private static Message EncryptMessage(string message, int columns)
+        {
+            string shortMsg = StripPunctuationFromString(message);
+            int rows = GetRowsNumber(shortMsg, columns);
+            int extraLetters = (rows * columns) - shortMsg.Count();
+            shortMsg += RandomString(extraLetters);
+            string encryptedMessage = string.Empty;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    encryptedMsg += matrix[j][i];
+                   encryptedMessage += shortMsg[j*rows+i];
                 }
             }
-            return encryptedMsg;
+            return new Message(encryptedMessage, extraLetters);
         }
 
-        //private static string DecryptMessage(string message, int columns)
-        //{
-        //    int rows = 0;
-        //    char[][] matrix = SetEncryptedMsgIntoColumns(message, columns, ref rows);
-        //    string decryptedMsg = string.Empty;
-        //    for (int i = 0; i < columns; i++)
-        //    {
-        //        for (int j = 0; j < rows; j++)
-        //        {
-        //            decryptedMsg += matrix[j][i];
-        //        }
-        //    }
-
-        //    return decryptedMsg;
-        //}
-
-        private static string SetOriginalMsgIntoColumns(string message, int columns, ref int rows)
+        private static string DecryptMessage(Message encryptedMessage, int columns)
         {
-            string shortMsg = StripPunctuationFromString(message);
-            int letters = shortMsg.Count();
-            rows = (int)Math.Ceiling((double)letters / (double)columns);
-            int extraLetters = (rows * columns) - letters;
-            shortMsg += RandomString(extraLetters);
-            return shortMsg;
-        }
-
-        private static string EncryptMessage(string message, int columns, int rows)
-        {
-            string shortMsg = StripPunctuationFromString(message);
-            int letters = shortMsg.Count();
-            rows = (int)Math.Ceiling((double)letters / (double)columns);
-            int extraLetters = (rows * columns) - letters;
-            shortMsg += RandomString(extraLetters);
-            string encryptedString = string.Empty;
-            for (int i = 0; i < rows; i++)
+            int rows = GetRowsNumber(encryptedMessage.msg, columns);
+            
+            string decryptedMessage = string.Empty;
+            for(int i = 0; i < columns; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < rows; j++)
                 {
-                   encryptedString += shortMsg[j*rows+i];
+                    decryptedMessage += encryptedMessage.msg[j * columns + i];
                 }
             }
-            return encryptedString;
+            if (encryptedMessage.extraChar != 0)
+                decryptedMessage = decryptedMessage.Remove(decryptedMessage.Length - encryptedMessage.extraChar);
+            return decryptedMessage;
         }
 
-        //private static char[][] CreateMatrixForDecryption(string message, int columns, int rows)
-        //{
-        //    char[][] matrix = new char[rows][];
-        //    int position = 0;
-        //    for (int i = 0; i < rows; i++)
-        //    {
-        //        matrix[i] = new char[columns];
-        //        for (int j = 0; j < columns; j++)
-        //        {
-        //            matrix[i][j] = message[position];
-        //            position++;
-        //        }
-        //    }
-        //    return matrix;
-        //}
+        private static int GetRowsNumber(string message, int columns)
+        {
+            int letters = message.Count();
+            int rows = (int)Math.Ceiling((double)letters / (double)columns);
+            return rows;
+        }
 
         private static string RandomString(int length)
         {
             const string chars = "abcdefghijklmnopqrstuvwxyz";
             var random = new Random();
-            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            string randomString = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                randomString += chars[random.Next(chars.Length)];
+            }
+            return randomString;
         }
 
         private static string StripPunctuationFromString(string message)
         {
-            return new string(message.Where(c => (!char.IsPunctuation(c) && c!=' ')).ToArray());
+            string shortMsg = string.Empty;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (char.IsLetterOrDigit(message[i]))
+                    shortMsg += message[i];
+            }
+            return shortMsg;
         }
     }
 }
